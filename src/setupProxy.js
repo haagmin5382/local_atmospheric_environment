a1b9945b1f0cf99ecd121e4e976f3432328bfd05
haagmin5382 <itworker5382@gmail.com>
2022-06-23 14:42:27 +0900

    fix urls

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/`,
+      target: `https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/`,
       changeOrigin: true,
       pathRewrite: {
         "^/api": "",
